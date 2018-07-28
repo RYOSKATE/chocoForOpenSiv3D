f050855b5a04feb011828005f6087e4401f6bfdb
Ryosuke Ishizue <ryoskate@gmail.com>
2018-07-28 15:59:59 +0900

    first commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://siv3d.jp/downloads/Siv3D/OpenSiv3D(0.2.7)Installer.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'open-siv3d*'
+  checksum      = '6AF41F722D55B6346B00CB589989DAF6B896A43165EAA63EDF5DAA79BF22028F'
+  checksumType  = 'sha256'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
